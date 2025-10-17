fe77bd534897f94aa86cac581ad40d83b2d37a55
fahrez182 <fahreza.raditya.busan@gmail.com>
2025-10-17 18:29:27 +0700

    Add: Settings to ViewModel

--- app/src/main/java/com/frb/axmanager/ui/viewmodel/ViewModelGlobal.kt
+++ app/src/main/java/com/frb/axmanager/ui/viewmodel/ViewModelGlobal.kt
@@ -2,6 +2,7 @@ package com.frb.axmanager.ui.viewmodel
 
 //IDK I'm just using it to make the code look compact
 class ViewModelGlobal(
+    val settingsViewModel: SettingsViewModel,
     val appsViewModel: AppsViewModel,
     val adbViewModel: AdbViewModel,
     val pluginViewModel: PluginViewModel
