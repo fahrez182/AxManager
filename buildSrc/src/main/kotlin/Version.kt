eb96009b69725b8377002448c9bc84788537ca7e
FahrezRB <fahreza.raditya.busan@gmail.com>
2025-12-27 23:01:42 +0700

    add: BuildSrc
    Prepare for managing constant

---
+++ buildSrc/src/main/kotlin/Version.kt
@@ -0,0 +1,9 @@
+object ServerVersion {
+    const val VERSION_CODE = 13118
+    const val PATCH_CODE = 0
+}
+
+object AppVersion {
+    const val VERSION_CODE = 13118
+    const val VERSION_NAME = "1.3.1"
+}
